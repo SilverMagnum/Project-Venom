c76b3983df74094809d9a55f657efa4546433849
TongueEnvy <mr.dfelkins@gmail.com>
2017-12-12 10:55:58 -0800

    Importing my new scene
    -Elevators
    -New test level model

---
+++ Project Venom/Assets/scr_glitchRandomizer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+using Kino;
+
+public class scr_glitchRandomizer : MonoBehaviour {
+
+    public float minGitchTimer;
+    public float maxGlitchTimer;
+    float glitchTimer;
+
+	// Use this for initialization
+	void Start () {
+
+        gameObject.GetComponent<Datamosh>().Glitch();
+        glitchTimer = Random.Range(minGitchTimer,maxGlitchTimer);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (glitchTimer > 0)
+        {
+
+            glitchTimer -= 1;
+
+        }
+        else if (glitchTimer <= 0)
+        {
+
+            glitchTimer = Random.Range(minGitchTimer, maxGlitchTimer);
+            gameObject.GetComponent<Datamosh>().Glitch();
+
+        }
+
+	}
+}
---
+++ Project Venom/Assets/scr_movingPlatform_updateDestination.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_movingPlatform_updateDestination : MonoBehaviour {
+
+    public GameObject platform;
+    public GameObject nextStop;
+
+    void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.gameObject == platform)
+        {
+
+            platform.GetComponent<scr_movingPlatform>().waitCounter = platform.GetComponent<scr_movingPlatform>().waitTimer;
+            platform.GetComponent<scr_movingPlatform>().newDestination = nextStop.transform.position;
+
+        }
+        
+
+    }
+}
